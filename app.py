8dc7c17c732ea3ed9ad054483cee64de61f5e50c
Rambod <gh.rambod@gmail.com>
2023-07-30 18:03:45 +0300

    "Add Flask routes and templates for Home and About pages"
    Explanation:
    
    The commit introduces Flask routes for the Home and About pages, handling requests to '/' and '/about' respectively.
    It also adds corresponding template files for the Home and About pages to display their content dynamically.
    Additionally, a basic navigation bar is included in the layout template to enable easy navigation between pages.
    The Home page displays a list of posts with their titles, content, author, and posting date dynamically from the 'posts' list in the Python code.
    The About page provides simple static content to showcase the template inheritance and dynamic rendering capabilities.

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Rambod Ghashghai',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 30, 2023'
+    },
+    {
+        'author': 'James Smith',
+        'title': 'Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 27, 2023'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return 'Hello World!'
+    return render_template('home.html', posts=posts, title='Home')
 
 @app.route('/about')
 def about():
-    return 'About'
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
